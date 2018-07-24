2fe08df35d24528aee491942d500929169c5d713
Tom King <18359588+tomkirish@users.noreply.github.com>
2018-07-24 11:10:49 -0600

    Updated deploy

--- azdeploy.tf
+++ azdeploy.tf
@@ -2,11 +2,30 @@
 provider "azurerm" {}
 
 # Create a resource group
-resource "azurerm_resource_group" "network" {
+resource "RG_Statera_Test" "network" {
   name     = "production"
   location = "West US"
 }
 
-#New Line
-#Another
+# Create a virtual network within the resource group
+resource "Statera_virtual_network" "network" {
+  name                = "Statera-production-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${RG_Statera_Test.network.location}"
+  resource_group_name = "${RG_Statera_Test.network.name}"
 
+  subnet {
+    name           = "Statera-subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "Statera-subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "Statera-subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
+}

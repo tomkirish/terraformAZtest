b9de5f76b515793178995332677f6c785382f173
Tom King <18359588+tomkirish@users.noreply.github.com>
2018-07-25 08:51:08 -0600

    changed params

--- azpocdeploy.tf
+++ azpocdeploy.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "network" {
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "network" {
-  name                = "Client-production-network"
+  name                = "Client-POC-network"
   address_space       = ["10.0.0.0/16"]
   location            = "${azurerm_resource_group.network.location}"
   resource_group_name = "${azurerm_resource_group.network.name}"
